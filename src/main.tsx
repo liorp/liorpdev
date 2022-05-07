2cbbce554f025c6813c7b1276e6d04c814e8b1a0
Lior Pollak <4294489+liorp@users.noreply.github.com>
2022-05-07 14:03:14 +0300

    feat: some color

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import 'virtual:windi.css'
 import './index.css'
 import App from './App'
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-)
+const container = document.getElementById('root')
+if (!container) throw new Error('Failed to find the root element')
+// Create a root.
+const root = createRoot(container)
+
+// Initial render
+root.render(<App />)
--- windi.config.ts
+++ windi.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig, transform } from 'windicss/helpers'
 
 export default defineConfig({
-    important: true,
     plugins: [transform('daisyui')],
 })
